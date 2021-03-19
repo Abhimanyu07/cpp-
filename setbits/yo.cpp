1275b1f3a1c9a1cf0558353a1434dc2f40fe2c0d
Abhimanyu07 <giyanopedia@gmail.com>
2021-03-19 19:05:40 +0530

    set bits done

--- setbits/yo.cpp
+++ setbits/yo.cpp
@@ -1,20 +1,18 @@
 #include<iostream>
 using namespace std;
+int return_setbit(int num)
+{
+int count = 0;
+for(long bit=31;bit>=0;bit--)
+{
+	long mask = (1L<<bit);
+	long ibit = mask&num;
+	if(ibit!=0) count++;
+}
+return count;
+}
 int main()
 {   
-    int i;
-    cout<<"enter a number: ";
-    // cin>>i;
-    i = 46; 
-    int count = 0;
-     for(long bit=31;bit>=0;bit--)
-    {
-long mask = (1L<<bit);
-long biti = mask&i;
-if(biti!=0) {cout<<"1"; count++;}
-else cout<<"0";
-if(bit==0) cout<<"\ninteger\n";
-
-    }
- cout<<endl<<count<<endl;
-    }
+int i=46;
+cout<<return_setbit(i);
+}
